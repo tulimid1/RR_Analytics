caf495391d7887393b3272e8b655ff3ec679712c
Duncan Tulimieri <tulimid@udel.edu>
2023-01-06 09:40:43 -0500

    add automatic run

--- dailyYouTubeScrape.py
+++ dailyYouTubeScrape.py
@@ -1,6 +1,18 @@
+import schedule
+import time
 from addChannelVideoInfo2Database import addChannelVideoInfo2Database
 from visualizeAllVideoViews import visualizeAllvideoViews
 
-addChannelVideoInfo2Database()
 
-visualizeAllvideoViews()
+def scrapeAndVisualize():
+    addChannelVideoInfo2Database()
+
+    visualizeAllvideoViews()
+
+
+schedule.every().day.at("9:00").do(scrapeAndVisualize)
+
+secondsPerDay = 86400
+while True:
+    schedule.run_pending()
+    time.sleep(secondsPerDay)
