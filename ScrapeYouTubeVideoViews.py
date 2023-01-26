e3a8b1b96e79b1ee0487d9c3dd3902184a38f7cd
Duncan Tulimieri <tulimid@udel.edu>
2023-01-26 17:24:24 -0500

    rewrite
    - modularize

--- ScrapeYouTubeVideoViews.py
+++ ScrapeYouTubeVideoViews.py
@@ -8,84 +8,81 @@ import string
 
 class ScrapeYouTubeVideoViews:
 
-    url = ""
-    title = ""
-    n_views = []
+    url = str
+    video_data = YouTube
+    _database_name = "YouTubeVideoData.db"
+    db_connection = sqlite3.Connection
+    db_cursor = sqlite3.Cursor
 
-    date_time_full = datetime.now()
-    capture_date = int(
-        str(date_time_full.year) + str(date_time_full.month) + str(date_time_full.day)
-    )
-
-    db_table_name = ""
-
-    def __init__(self, video_url):
+    def __init__(self, video_url: str, database_name: str = "YouTubeVideoData.db"):
         self.url = video_url
-        video_data = YouTube(self.url)
-        self.title = video_data.title
-        self.set_db_table_name()
-        self.n_views = video_data.views
+        self.video_data = YouTube(self.url)
+        self.database_name = database_name
 
-    def set_db_table_name(self):
+    def convert_title_to_machine_readable(self) -> str:
         subStr = "[%s ]" % string.punctuation
-        self.db_table_name = '_' + re.sub(subStr, "_", self.title)
+        return "_" + re.sub(subStr, "_", self.get_video_title())
 
-    def addData2DataBase(self):
-        conn = self.connect2database()
-        cursor = conn.cursor()
+    def get_video_views(self) -> int:
+        return self.video_data.views
 
-        tableExists = self.determineIfTableExists(cursor)
-        if not tableExists:
-            cursor.execute(
-                "CREATE TABLE "
-                + self.db_table_name
-                + " (capture_date integer, views integer)"
-            )
+    def get_video_title(self) -> str:
+        return self.video_data.title
 
-        cursor.execute(
-            "INSERT INTO " + self.db_table_name + " VALUES (?, ?)",
+    def get_capture_date_str(self, capture_datetime: datetime = datetime.now()) -> str:
+        capture_date_str = capture_datetime.strftime("%Y-%m-%d")
+        return capture_date_str
+
+    def table_exists_in_db(self, table_name: str) -> bool:
+        return table_name in self.get_all_table_names_in_db()
+
+    def get_all_table_names_in_db(self) -> list:
+        list_of_all_table_info = self.db_cursor.execute(
+            "SELECT * FROM sqlite_master where type='table'"
+        ).fetchall()[:]
+        table_names = [table_info[1] for table_info in list_of_all_table_info]
+        return table_names
+
+    def add_table_to_db(self, table_name: str):
+        self.db_cursor.execute(
+            "CREATE TABLE " + table_name + " (capture_date text, views integer)"
+        )
+        self.db_connection.commit()
+
+    def get_connection_cursor(self) -> sqlite3.Cursor:
+        return self.db_connection.cursor()
+
+    def connect_to_database(self) -> sqlite3.Connection:
+        return sqlite3.connect(self.database_name)
+
+    def add_scrape_data_to_db_table(self, table_name: str):
+        self.db_cursor.execute(
+            "INSERT INTO " + table_name + " VALUES (?, ?)",
             (
-                self.capture_date,
-                self.n_views,
+                self.get_capture_date_str(),
+                self.get_video_views(),
             ),
         )
+        self.db_connection.commit()
 
-        conn.commit()
-        conn.close()
-
-    def connect2database(self):
-        return sqlite3.connect("YouTubeVideoData.db")
-
-    def determineIfTableExists(self, cursor):
-        listOfTables = cursor.execute(
-            """SELECT name FROM sqlite_master WHERE type='table' AND name=?; """,
-            (self.db_table_name,),
-        ).fetchall()
-
-        if listOfTables == []:
-            return False
+    def get_scrape_data_from_db_table(self, table_name: str) -> pd.DataFrame:
+        table_data = self.db_cursor.execute("SELECT * FROM " + table_name).fetchall()
+        if table_data == []:
+            return pd.DataFrame()
         else:
-            return True
+            column_names_full = self.db_cursor.description
+            column_names = [c_name[0] for c_name in column_names_full]
+            df = pd.DataFrame(data=table_data, columns=column_names)
+            df["capture_date"] = pd.to_datetime(df["capture_date"], format="%Y-%m-%d")
+            df = df.set_index("capture_date")
+            return df
 
-    def extractDataFromDataBase(self):
-        conn = self.connect2database()
-        cursor = conn.cursor()
-        cursor.execute("SELECT * FROM " + self.db_table_name)
-        df = self.data2pdDataFrame(cursor)
-        return df
+    @property
+    def databse_name(self):
+        return self._database_name
 
-    def data2pdDataFrame(self, cursor):
-        column_names = cursor.description
-        result = [
-            {column_names[index][0]: column for index, column in enumerate(value)}
-            for value in cursor.fetchall()
-        ]
-        df = pd.DataFrame(result)
-        df["capture_date"] = pd.to_datetime(df["capture_date"], format="%Y%m%d")
-        df = df.set_index("capture_date")
-        return df
-
-    def __str__(self) -> str:
-        date = datetime.strptime(str(self.capture_date), "%Y%m%d")
-        month, day, year = date.month, date.day, date.year
-        return f"Video: '{self.title}' has {self.n_views:.0f} views on {month}-{day}-{year}.\n"
+    @databse_name.setter
+    def database_name(self, new_name: str) -> str:
+        self._database_name = new_name
+        self.db_connection = self.connect_to_database()
+        self.db_cursor = self.get_connection_cursor()
